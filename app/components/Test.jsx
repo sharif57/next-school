f8ce91f779a51eeb88139d978f9419df999994ba
sharif57 <sharifmahamud577951@gmail.com>
2024-09-17 22:26:38 +0600

    Navigate section done

---
+++ app/components/Test.jsx
@@ -0,0 +1,70 @@
+// import 'swiper/css';
+// import 'swiper/css/free-mode';
+// import 'swiper/css/pagination';
+// import 'swiper/css/navigation';
+
+
+// // Import Swiper and SwiperSlide from swiper/react
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import { Pagination, Navigation } from 'swiper/modules';
+
+
+// // Import required modules
+// // import { FreeMode, Pagination } from 'swiper/modules';
+// const Test = () => {
+//   return <div>
+//      <div className="mt-[130px] px-8">
+//             <div className="w-1/2 mx-auto text-center space-y-4 ">
+//                 <p className="btn btn-outline text-black font-semibold">
+//                     Their Happy Words 🤗
+//                 </p>
+//                 <h1 className="text-5xl font-bold">Our Testimonials</h1>
+//                 <p>
+//                     Our testimonials are heartfelt reflections of the nurturing
+//                     environment we provide, where children flourish both
+//                     academically and emotionally.
+//                 </p>
+//             </div>
+//             <div className='mt-12'>
+//                 <Swiper
+//                     slidesPerView={3}
+//                     spaceBetween={30}
+//                     loop={true}
+//                     pagination={{
+//                         clickable: true,
+//                     }}
+//                     navigation={true}
+//                     modules={[Pagination, Navigation]}
+//                     className="mySwiper "
+//                 >
+//                     <SwiperSlide>
+//                         <div className="flex w-full p-4  flex-col rounded-lg bg-white shadow-sm border-2  border-slate-500 my-6 hover:shadow-2xl hover:border-b-8 hover:border-r-8 hover:border-black">
+//                             <div className="flex flex-col items-center gap-4 text-slate-800">
+//                                 {/* <img src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1480&amp;q=80" alt="Tania Andrew" className="relative inline-block h-[58px] w-[58px] !rounded-full  object-cover object-center" /> */}
+//                                 <div className="flex w-full flex-col">
+//                                     <div className="flex flex-col items-center justify-between">
+//                                         <h5 className="text-xl font-semibold text-slate-800 mb-6">
+//                                             Tania Andrew
+//                                         </h5>
+                                       
+//                                     </div>
+
+//                                 </div>
+//                             </div>
+//                             <div className="mt-6">
+//                                 <p className="text-base text-slate-600 font-light leading-normal">
+//                                     &quot;I found solution to all my design needs from Creative Tim. I use
+//                                     them as a freelancer in my hobby projects for fun! And its really
+//                                     affordable, very humble guys !!!&quot;
+//                                 </p>
+//                             </div>
+//                         </div>
+//                     </SwiperSlide>
+                    
+                    
+//                 </Swiper>
+//             </div>
+//         </div>
+//   </div>;
+// };
+// export default Test;
--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
 import Banner from "./components/Banner";
 import Benefits from "./components/Benefits";
+import Frequently from "./components/Frequently";
+import Test from "./components/Test";
 
 export default function Home() {
   return (
     <div className="">
       <Banner></Banner>
       <Benefits></Benefits>
+      {/* <Test></Test> */}
+      <Frequently></Frequently>
     </div>
   );
 }
