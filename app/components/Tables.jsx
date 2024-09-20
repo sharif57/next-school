b6f9998edf9d3fb3617792830dbbc74bba46ccaf
sharif57 <sharifmahamud577951@gmail.com>
2024-09-20 19:06:19 +0600

    tables data show

--- app/components/Tables.jsx
+++ app/components/Tables.jsx
@@ -1,43 +1,50 @@
 const Tables = () => {
-  return <div>
-    <div className="container mx-auto p-4">
-      <h2 className="text-2xl font-bold mb-4">HTML Table</h2>
-      <table className="min-w-full table-auto border-collapse border border-gray-300">
-        <thead>
-          <tr className="bg-gray-800 text-white">
-            <th className="border border-gray-300 px-4 py-2">Program</th>
-            <th className="border border-gray-300 px-4 py-2">Age Group</th>
-            <th className="border border-gray-300 px-4 py-2">Annual Tuition</th>
-            <th className="border border-gray-300 px-4 py-2">Registration Fee</th>
-            <th className="border border-gray-300 px-4 py-2">Activity Fee</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr className="bg-gray-100">
-            <td className="border border-gray-300 px-4 py-2">Nursery</td>
-            <td className="border border-gray-300 px-4 py-2">2 - 3 Years</td>
-            <td className="border border-gray-300 px-4 py-2">$1,686</td>
-            <td className="border border-gray-300 px-4 py-2">$162</td>
-            <td className="border border-gray-300 px-4 py-2">$12</td>
-          </tr>
-          <tr>
-            <td className="border border-gray-300 px-4 py-2"> Pre - Kindergartens</td>
-            <td className="border border-gray-300 px-4 py-2">3 - 4 Years</td>
-            <td className="border border-gray-300 px-4 py-2">$2,686</td>
-            <td className="border border-gray-300 px-4 py-2">$16</td>
-          </tr>
-          <tr className="bg-gray-100">
-            <td className="border border-gray-300 px-4 py-2">Kindergarten</td>
-            <td className="border border-gray-300 px-4 py-2">4 - 5 Years</td>
-            <td className="border border-gray-300 px-4 py-2">$3,686</td>
-            <td className="border border-gray-300 px-4 py-2">$340</td>
-            <td className="border border-gray-300 px-4 py-2">$20</td>
-          </tr>
-         
-        </tbody>
-      </table>
+  return <div className="mt-[130px] ">
+    <div className="w-1/2 mx-auto text-center space-y-4 ">
+      <p className="btn btn-outline text-black font-semibold">Our Features</p>
+      <h1 className="text-5xl  font-bold">Fee Structure</h1>
+      <p>Our fee structure is transparent, and we strive to keep our fees competitive within the education sector. The fees vary based on the program, age group, and any additional services chosen.</p>
     </div>
-  
+    <div className="container mx-auto p-8 mt-10">
+      <div className="border-2 border-r-8 border-l-8 border-black rounded-lg p-8">
+        <table className="min-w-full table-auto border-collapse border border-gray-300 text-center">
+          <thead className="">
+            <tr className="bg-gray-800 text-white">
+              <th className="border border-gray-300 px-4 py-2">Program</th>
+              <th className="border border-gray-300 px-4 py-2">Age Group</th>
+              <th className="border border-gray-300 px-4 py-2">Annual Tuition</th>
+              <th className="border border-gray-300 px-4 py-2">Registration Fee</th>
+              <th className="border border-gray-300 px-4 py-2">Activity Fee</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="bg-gray-100">
+              <td className="border border-gray-300 px-4 py-2">Nursery</td>
+              <td className="border border-gray-300 px-4 py-2">2 - 3 Years</td>
+              <td className="border border-gray-300 px-4 py-2">$1,686</td>
+              <td className="border border-gray-300 px-4 py-2">$162</td>
+              <td className="border border-gray-300 px-4 py-2">$12</td>
+            </tr>
+            <tr>
+              <td className="border border-gray-300 px-4 py-2"> Pre - Kindergartens</td>
+              <td className="border border-gray-300 px-4 py-2">3 - 4 Years</td>
+              <td className="border border-gray-300 px-4 py-2">$2,686</td>
+              <td className="border border-gray-300 px-4 py-2">$16</td>
+              <td className="border border-gray-300 px-4 py-2">$20</td>
+            </tr>
+            <tr className="bg-gray-100">
+              <td className="border border-gray-300 px-4 py-2">Kindergarten</td>
+              <td className="border border-gray-300 px-4 py-2">4 - 5 Years</td>
+              <td className="border border-gray-300 px-4 py-2">$3,686</td>
+              <td className="border border-gray-300 px-4 py-2">$340</td>
+              <td className="border border-gray-300 px-4 py-2">$20</td>
+            </tr>
+
+          </tbody>
+        </table>
+      </div>
+    </div>
+
   </div>;
 };
 export default Tables;
